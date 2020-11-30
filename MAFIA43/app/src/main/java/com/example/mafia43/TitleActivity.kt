bcb05f6d56980ea28a48aac5c9acfa9291c6523c
Juliian Travis <jtravis2@umd.edu>
2020-11-30 00:12:34 -0500

    Fixed a typo and setup for next activity

--- MAFIA43/app/src/main/java/com/example/mafia43/TitleActivity.kt
+++ MAFIA43/app/src/main/java/com/example/mafia43/TitleActivity.kt
@@ -2,10 +2,23 @@ package com.example.mafia43
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class TitleActivity : AppCompatActivity() {
+    companion object {
+
+    }
+
+    private lateinit var mPlayButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_title)
+
+        mPlayButton = findViewById(R.id.play_button)
+
+        mPlayButton.setOnClickListener {
+            // Start the next activity!
+        }
     }
 }
