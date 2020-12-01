803577735cd0607792ec5c48300c0ccee66a9b52
Naomi Perez <naomiperez@Naomis-MacBook-Air-798.local>
2020-12-01 12:08:22 -0500

    just added a confirm button

--- MAFIA43/app/src/main/java/com/example/mafia43/Voting.kt
+++ MAFIA43/app/src/main/java/com/example/mafia43/Voting.kt
@@ -11,23 +11,38 @@ import com.example.mafia43.R
 import kotlinx.android.synthetic.main.voting_selection.*
 import android.util.Log
 
+
 class Voting : AppCompatActivity() {
     lateinit var extras : Bundle
     lateinit var mInflater: LayoutInflater
     lateinit var votingView : TextView
+    lateinit var mPlayers : Array<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.voting_selection)
 
-        Log.i("tag", "here")
         /* Get players array from intent */
-        extras = intent.extras!!
+        mPlayers = intent.getStringArrayExtra("playersArr") as Array<String>
+
+        for(i in 0 until mPlayers.size){
+            Log.i(TAG,  "player ${mPlayers[i]}")
+        }
 
         /* Create layout inflater for selection of players */
         mInflater = applicationContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
         mInflater.inflate(R.layout.voting_selection, player_list, false)
+
+        //addPlayersToList(mInflater, e)
         
+    }
+
+    fun addPlayersToList(inflater: LayoutInflater, players: Array<String>){
+
+    }
+
+    companion object{
+        const val TAG = "tag"
     }
 }
