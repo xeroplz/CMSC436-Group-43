87de64a37a36477f2d0995cf0d3074a8aea4e466
Matt Corrado <mattjbcorrado@Sputnik.local>
2020-12-02 19:17:57 -0500

    Added night activity, Added option in temp to go to night, tried to merge code

---
+++ MAFIA43/app/src/main/java/com/example/mafia43/NightActivity.kt
@@ -0,0 +1,15 @@
+package com.example.mafia43
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NightActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.night_role_confirm)
+
+
+
+
+    }
+}
--- MAFIA43/app/src/main/java/com/example/mafia43/Temp.kt
+++ MAFIA43/app/src/main/java/com/example/mafia43/Temp.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
+import java.io.Serializable
 
 /* I created this as an placeholder activity so that I can use it to
  guide my implementation of Voting.kt */
@@ -11,31 +12,45 @@ import android.content.Intent
 class Temp: AppCompatActivity()  {
 
     /* "players" Array will represent the array of player names we get from initial activity */
-    lateinit var players : Array<String>
+    lateinit var players : Array<Player>
 
     private lateinit var mVoteButton : Button
+    private lateinit var mNightButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.temp)
 
         mVoteButton = findViewById(R.id.vote_button)
+        mNightButton = findViewById(R.id.night_button)
 
         /* Just placeholder strings for now */
-        players = arrayOf<String>(
-            "Naomi",
-            "Julian",
-            "Matt",
-            "A name",
-            "Another",
-            "Player 3"
+        players = arrayOf<Player>(
+            Player("Naomi", 1),
+            Player("Julian", 2),
+            Player("Matt", 3),
+            Player("A name", 4),
+            Player("Another", 4),
+            Player("Player 3", 4)
         )
 
         mVoteButton.setOnClickListener{
             /* Creating intent for Voting Activity using players data.
             Add players array as extra to intent. */
             val voteIntent = Intent(this@Temp, Voting::class.java)
-            voteIntent.putExtra("playersArr", players)
+            /* You have to create a Bundle to pass the Player array */
+            val args = Bundle()
+            args.putSerializable("playersArr", players as Serializable)
+            voteIntent.putExtra("Bundle", args)
             startActivity(voteIntent)
+        }
+
+        mNightButton.setOnClickListener{
+            val nightIntent = Intent(this@Temp, NightActivity::class.java)
+            /* You have to create a Bundle to pass the Player array */
+            val args = Bundle()
+            args.putSerializable("playersArr", players as Serializable)
+            nightIntent.putExtra("Bundle", args)
+            startActivity(nightIntent)
         }
 
     }
