b8a2251703461bd387fdb3662c725a3826c6d11c
Naomi Perez <naomiperez@Naomis-MacBook-Air-798.local>
2020-12-04 16:42:25 -0500

    voting recap goes to night activity

--- MAFIA43/app/src/main/java/com/example/mafia43/VoteRecapActivity.kt
+++ MAFIA43/app/src/main/java/com/example/mafia43/VoteRecapActivity.kt
@@ -1,12 +1,15 @@
 package com.example.mafia43
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class VoteRecapActivity : AppCompatActivity() {
     lateinit var votedOut : String
     lateinit var mTextView : TextView
+    lateinit var continueButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +20,19 @@ class VoteRecapActivity : AppCompatActivity() {
         votedOut = intent.getStringExtra("VotedOff").toString()
 
         mTextView.text = "$votedOut was voted off."
+
+        val nightIntent = Intent(this@VoteRecapActivity, NightActivity::class.java)
+        val args = Bundle()
+        args.putSerializable("playersArr", intent.getSerializableExtra("playersArr"))
+
+        continueButton = findViewById(R.id.recap_next_button)
+
+        continueButton.setOnClickListener {
+            nightIntent.putExtra("Bundle", args)
+            nightIntent.putExtra("AlivePlayers", intent.getStringExtra("AlivePlayers"))
+            nightIntent.putExtra("Role", 1)
+            nightIntent.putExtra("Kill", "")
+            nightIntent.putExtra("Save", "")
+        }
     }
 }
