b7158a1c89f2b098ab97513fce17855049e75648
Matt Corrado <mattjbcorrado@Sputnik.local>
2020-12-04 16:14:14 -0500

    Finished night time phase and added sound effects

--- MAFIA43/app/src/main/java/com/example/mafia43/NightActivity.kt
+++ MAFIA43/app/src/main/java/com/example/mafia43/NightActivity.kt
@@ -2,7 +2,10 @@ package com.example.mafia43
 
 import android.content.Context
 import android.content.Intent
+import android.media.AudioAttributes
+import android.media.AudioFocusRequest
 import android.media.AudioManager
+import android.media.SoundPool
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -10,18 +13,22 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import java.io.Serializable
+import java.util.concurrent.Semaphore
 
 
 class NightActivity : AppCompatActivity() {
     private lateinit var mPlayers : Array<Player>
     private lateinit var mBundle : Bundle
     private lateinit var mTextView : TextView
-    private lateinit var mAudioManager: AudioManager
     private lateinit var mHandler: Handler
     private lateinit var mContinueButton: Button
+    private lateinit var mAudioManager: AudioManager
+    private lateinit var mSoundPool: SoundPool
+    private var mSoundId: Int = 0
     private var role = MAFIA
-    private var vol = 0.5f
 
+    // Audio volume
+    private var mStreamVolume: Float = 0.toFloat()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,9 +37,13 @@ class NightActivity : AppCompatActivity() {
         mBundle = intent.getBundleExtra("Bundle")!!
         mPlayers = mBundle.getSerializable("playersArr") as Array<Player>
         mTextView = findViewById(R.id.nRoleTextView)
-        mAudioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
         mHandler = Handler(Looper.getMainLooper())
         mContinueButton = findViewById(R.id.nContinue)
+
+        // Get reference to the AudioManager
+        mAudioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
+        this.volumeControlStream = AudioManager.STREAM_MUSIC
 
         role =  intent.getIntExtra("Role", MAFIA)
         val alive = intent.getIntExtra("AlivePlayers", mPlayers.size)
@@ -46,7 +57,7 @@ class NightActivity : AppCompatActivity() {
                     if(player.role() == MAFIA) {
                         if(player.alive()) {
                             mTextView.text = "Mafia Awake..."
-                            mHandler.postDelayed(mRunnable, 3000)
+                            mHandler.postDelayed(mRunnable, 1000)
                         } else {
                             /* send to Mafia loss screen */
                         }
@@ -78,12 +89,18 @@ class NightActivity : AppCompatActivity() {
             3 -> {
                 for (player in mPlayers) {
                     if(player.role() == DETECTIVE) {
+                        for(player in mPlayers) {
+                            if (player.role() == NightRecapActivity.DOCTOR) {
+                                if(player.alive()) {
+                                    mHandler.postDelayed(mRunnable, 4000)
+                                } else {
+                                    mHandler.postDelayed(mRunnable, 10000)
+                                }
+                            }
+                        }
                         if(player.alive()) {
                             mTextView.text = "Detective Awake..."
-                            mHandler.postDelayed(mRunnable, 8000)
                         } else {
-                            mHandler.postDelayed(mRunnable, 8000)
-
                             /* Send to night recap page */
                             val nightIntent = Intent(this@NightActivity, NightRecapActivity::class.java)
                             /* You have to create a Bundle to pass the Player array */
@@ -115,10 +132,32 @@ class NightActivity : AppCompatActivity() {
     }
 
     private val mRunnable = Runnable {
+        mStreamVolume = mAudioManager!!
+            .getStreamVolume(AudioManager.STREAM_MUSIC).toFloat() / mAudioManager!!.getStreamMaxVolume(
+            AudioManager.STREAM_MUSIC
+        )
+
+        val audioAttributes = AudioAttributes.Builder()
+            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+            .build()
+
+        mSoundPool = SoundPool.Builder()
+            .setMaxStreams(10)
+            .setAudioAttributes(audioAttributes)
+            .build()
+
+        mSoundPool.setOnLoadCompleteListener { soundPool, sampleId, status ->  mSoundPool.play(mSoundId, mStreamVolume, mStreamVolume, 1, 0, 1f) }
+
         when (role) {
-            MAFIA -> mAudioManager.playSoundEffect(AudioManager.FX_KEYPRESS_INVALID, vol)
-            DOCTOR -> mAudioManager.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR, vol)
-            DETECTIVE -> mAudioManager.playSoundEffect(AudioManager.FX_KEYPRESS_RETURN, vol)
+            MAFIA -> {
+                mSoundId = mSoundPool.load(this@NightActivity, R.raw.mafia, 1)
+            }
+            DOCTOR -> {
+                mSoundId = mSoundPool.load(this@NightActivity, R.raw.doctor, 1)
+            }
+            DETECTIVE -> {
+                mSoundId = mSoundPool.load(this@NightActivity, R.raw.detective, 1)
+            }
         }
     }
 
