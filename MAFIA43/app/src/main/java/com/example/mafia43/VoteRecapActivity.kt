19c1b6ac77d10f38733c7bd77aa82318966a61b8
Naomi Perez <naomiperez@Naomis-MacBook-Air-798.local>
2020-12-06 14:31:27 -0500

    added exit button to voting activities

--- MAFIA43/app/src/main/java/com/example/mafia43/VoteRecapActivity.kt
+++ MAFIA43/app/src/main/java/com/example/mafia43/VoteRecapActivity.kt
@@ -1,11 +1,14 @@
 package com.example.mafia43
 
+import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 
 class VoteRecapActivity : AppCompatActivity() {
     lateinit var votedOut : String
@@ -40,5 +43,27 @@ class VoteRecapActivity : AppCompatActivity() {
 
             startActivity(nightIntent)
         }
+
+    }
+
+    override fun onBackPressed() {
+        //super.onBackPressed()
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this@VoteRecapActivity)
+
+        builder.setTitle("Quit Game")
+        builder.setMessage("Are you sure you want to end the game?")
+        builder.setNegativeButton("Yes", DialogInterface.OnClickListener{ _, _ ->
+            val restartIntent = Intent(this@VoteRecapActivity, TitleActivity::class.java)
+            startActivity(restartIntent)
+        })
+        builder.setPositiveButton("No", null)
+
+        val alertDialog = builder.create()
+        alertDialog.setCanceledOnTouchOutside(false)
+        alertDialog.setOnShowListener {
+            alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(Color.GRAY)
+            alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.GRAY)
+        }
+        alertDialog.show()
     }
 }
